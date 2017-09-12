d4dc159442731a17933807c76eba087f19a7deb0
Elena Morton <elena.uleia@gmail.com>
2017-09-12 14:06:17 +0000

    adding test for removing mixed case letters

--- test/app.js
+++ test/app.js
@@ -6,18 +6,25 @@ let app = new App();
 
 let inputStr = 'AAAc91%cWwWkLq$1ci3_848v3d__K';
 let inputStrLower = 'aacccd91ee';
-let inputStrUpper = 'AAACC91EE'
+let inputStrUpper = 'AAACC91EE';
+let inputStrMixedCase = 'AAAcc91EEff';
 
 describe('App', () => {
     
     describe('removes duplicated characters from a string', () => {
-        it('removes lower case chars', () => {
+        it('removes lower case letters', () => {
            expect(app.removeDuplicateChars(inputStrLower)).to.equal('acd91e'); 
         })
         
-        it('removes lower case chars', () => {
+        it('removes upper case letters', () => {
            expect(app.removeDuplicateChars(inputStrUpper)).to.equal('AC91E'); 
         })
+        
+        it('removes mixed case letters', () => {
+           expect(app.removeDuplicateChars(inputStrMixedCase)).to.equal('Ac91Ef'); 
+        })
+        
+        
         
     })
 })
