63a03464749c748e84d53ca7ac6ee4560c07cedb
Elena Morton <elena.uleia@gmail.com>
2017-09-12 16:04:30 +0000

    implementing the replaceChars function

--- app/app.js
+++ app/app.js
@@ -10,6 +10,11 @@ class App {
         var re = new RegExp(extraChars, "g")
         return str.replace(re, "")
     }
+    
+    replaceChars(str, charToBeChanged, charThatChange) {
+        var re = new RegExp('[' + charToBeChanged + ']', "g");
+        return str.replace(re, charThatChange)
+    }
 }
 
 module.exports=App;
