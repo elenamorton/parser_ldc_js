f5c26ada69015d6bdc20ac3b2b7df92b5fd0f961
Elena Morton <elena.uleia@gmail.com>
2017-09-12 14:51:41 +0000

    implementing the removeExtraChars function by creating new RegExp object

--- app/app.js
+++ app/app.js
@@ -2,7 +2,13 @@
 class App {
     
     removeDuplicateChars(str) {
-        return str.replace(/(.)(?=\1)/g, "")
+        return str.replace(/(.)(?=\1)/g, "");
+    }
+    
+    removeExtraChars(str, arrCharsToRemove) {
+        var extraChars = arrCharsToRemove.join('');
+        var re = new RegExp(extraChars, "g")
+        return str.replace(re, "")
     }
 }
 
